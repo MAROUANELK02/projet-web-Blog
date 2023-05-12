c2eac26703ff15f98e178bd71195310d0de952a8
Marouane LK <marouanelk02@gmail.com>
2023-05-12 20:34:25 +0100

    front end with react

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+    <div className="post">
+    <div className="image">
+        <img src="https://www.shutterstock.com/image-vector/chain-link-icon-vector-260nw-241708024.jpg"></img>
+        </div>
+        <div className="texts">
+        <h2>Article n°251</h2>
+        <p className="info">
+          <a className="author">Dawid Paszko</a>
+          <time>2023-01-06</time>
+        </p>
+        <p className="summary">Article ichihcoisjcijdsojcsdjcsodcsjcsojkôpkdc</p>
+      </div>
+    </div>
+    )
+}
